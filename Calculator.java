094a5184bb850c4f3e71445adf9b486750407e84
Saurabh Daware <saurabhdaware2001@gmail.com>
2020-07-27 15:53:08 +0530

    OOP practice

--- Calculator.java
+++ Calculator.java
@@ -1,43 +1,44 @@
 package conditionalStatement;
-import java.util.Scanner;
+import java.io.*;
 
 public class Calculator {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter first number: ");
-		int a = sc.nextInt();
-		System.out.println("Enter second number: ");
-		int b = sc.nextInt();
-		
-		System.out.println("Enter the operation: ");
-		sc.nextLine();
-		char operation = sc.nextLine().charAt(0);
-		int result = 0;
-		
-		switch (operation) {
-		case '+':
-			result = a + b ;
-			break;
-		case '-':
-			result = a - b;
-			break;
-		case '*':
-			result = a * b ;
-			break;
-		case '/':
-			result = a /b;
-			break;
-			
-		default:
-		System.out.println("Invalid operation");
-			
-		}
-		System.out.println("The result is " + result);
-		
+	public static void main(String[] args) throws IOException {
 
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter First number");
+		double num1 = Double.parseDouble(reader.readLine());
+		System.out.println("Enter second nuber");
+		double num2 = Double.parseDouble(reader.readLine());
+		System.out.println("Enter + for Addition \n"+"- for Subraction\n"+"* for Multiplication\n"+"/ for Division");
+		String input = reader.readLine();
+		char operation = input.charAt(0);
+		Condition(num1,num2,operation);
 	}
 
+	private static void Condition(double num1, double num2,char operation) {
+		Operation oop = new Operation();
+
+		switch(operation) {
+		
+		case '+' :System.out.println("The sum of "+num1 +" and "+num2+" is "+DoAddition(num1,num2));
+			break;
+		
+		case '-' :System.out.println("The sum of "+num1 +" and "+num2+" is "+DoSubract(num1,num2));
+		break;
+		
+		case '*' :System.out.println("The sum of "+num1 +" and "+num2+" is "+DoMultiply(num1,num2));
+		break;
+		
+		case '/' :System.out.println("The sum of "+num1 +" and "+num2+" is "+DoDivision(num1,num2));
+		break;
+		}
+		
+	}
+
+	static double DoDivision(double a,double b) {return a/b;}
+	static double DoMultiply(double a,double b) {return a*b;}
+	static double DoAddition(double a,double b) {return a+b;}
+	static double DoSubract(double a,double b) {return a-b;}
 }
--- GreatestNumber.java
+++ GreatestNumber.java
@@ -6,18 +6,24 @@ public class GreatestNumber {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		int a,b,c,result;
+		int a,b,c;
+		System.out.println("Enter first number");
+		a = sc.nextInt();
+		System.out.println("Enter second number");
+		b = sc.nextInt();
+		System.out.println("Enter third number");
+		c = sc.nextInt();
+		
+		Comparison(a,b,c);
+	}
 
-		 a = sc.nextInt();
-		 b = sc.nextInt();
-		 c = sc.nextInt();
-		 
+	private static void Comparison(int a, int b, int c) {
+		 int result;
 		
 		result = b > a ? b > c ? b : c : a > c ? a : c;
 		
-		System.out.println(result);
+		System.out.println("Greatest Number is "+result);
 		
 	}
-	
 
 }
