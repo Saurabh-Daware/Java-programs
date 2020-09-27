bf14f3f23fd4da63e1d96552559be17b16affca9
Saurabh Daware <65069121+saurabh-daware@users.noreply.github.com>
2020-09-27 22:30:28 +0530

    Update ReviewBySwitchCaseStatement.java

--- ReviewBySwitchCaseStatement.java
+++ ReviewBySwitchCaseStatement.java
@@ -9,20 +9,20 @@ public class ReviewBySwitchCaseStatement {
 	int rating = sc.nextInt();
 	
 	switch (rating) {
-	case 1:
-	case 2:
-		System.out.println("Bad review");
-		break;
-	case 3:
-		System.out.println("Average review");
-		break;
-	case 4:
-	case 5:
-		System.out.println("Good review");
-		break;
-	}
-	
+		case 1:
+		case 2:
+			System.out.println("Bad review");
+			break;
+		case 3:
+			System.out.println("Average review");
+			break;
+		case 4:
+		case 5:
+			System.out.println("Good review");
+			break;
 		}
-		
+	
 	}
+		
+}
 
