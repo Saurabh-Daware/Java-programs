00b94f5e41336333cba3e74a70861ede0cb26cbb
bishalmalakar <71222182+bishalmalakar@users.noreply.github.com>
2020-10-31 21:30:09 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -8,7 +8,7 @@ public class LeapYear {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Enter a year: ");
+		System.out.println("Enter year: ");
 		
 		int year = sc.nextInt();
 		
