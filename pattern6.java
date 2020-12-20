e8ef84b2d411f5ff8e54bacf53f2604ea030fe56
Saurabh Daware <saurabhdaware2001@gmail.com>
2020-12-20 16:43:33 +0530

    Added pattern6.java

---
+++ pattern6.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class pattern6 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j< 4;j++) {
+				if(i == j) System.out.print("O ");
+				else System.out.print("X ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
