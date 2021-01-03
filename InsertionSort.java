43dcd0191230c9ad51ba97c95b70407ca4944caf
Saurabh Daware <saurabhdaware2001@gmail.com>
2021-01-03 18:32:50 +0530

    Added InsertSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,26 @@
+package algorithms;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		
+		int a[] = {6, 4, 35, 2, 1, -4, 23, 120, 3};
+		
+		int len = a.length;
+		
+		for(int i = 1; i < len; ++i) {
+			int key = a[i];
+			int j = i - 1;
+			
+			while(j >= 0 && a[j] > key) {
+				a[j+1] = a[j];
+				j = j - 1;
+			}
+			a[j + 1] = key;
+		}
+		for (int items : a) {
+			System.out.print(" " + items);
+		}
+	}
+
+}
