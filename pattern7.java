251811453350fbc14f138f2100e8297867500cc9
Saurabh Daware <saurabhdaware2001@gmail.com>
2021-04-06 21:00:40 +0530

    added user input in pattern7.java

--- pattern7.java
+++ pattern7.java
@@ -1,8 +1,12 @@
+import java.util.*;
+
 public class Pattern7 {
 
 	public static void main(String[] args) {
 
-		int n = 5;
+		int n;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
 		for (int i = 0; i <= n; ++i) {
 
 			for (int j = 0; j < (n - i); ++j) {
